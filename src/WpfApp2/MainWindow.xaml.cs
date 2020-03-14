59abd1489c56d377ec3a6f597091e61d8699cdb1
punker76 <punkerat76@gmail.com>
2020-03-14 23:06:54 +0100

    Add sample

---
+++ src/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+
+namespace WpfApp2
+{
+  /// <summary>
+  /// Interaction logic for MainWindow.xaml
+  /// </summary>
+  public partial class MainWindow : MetroWindow
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+
+    public static readonly DependencyProperty MyPropertyProperty = DependencyProperty.Register(
+        "MyProperty", typeof(string), typeof(MainWindow), new PropertyMetadata(default(string)));
+
+    public string MyProperty
+        {
+        get { return (string) GetValue(MyPropertyProperty); }
+        set { SetValue(MyPropertyProperty, value); }
+    }
+  }
+}
